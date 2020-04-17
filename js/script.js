89b70b087efee5ae11bad0ddf6cb030c012163d2
pluisx <pluisx@gmail.com>
2020-04-16 21:10:28 -0300

    commit agregue nuevo sticky menu

--- js/script.js
+++ js/script.js
@@ -3,14 +3,7 @@
 
 
 
-    // Sticky Menu
-    $(window).scroll(function () {
-        if ($('.navigation').offset().top > 100) {
-            $('.navigation').addClass('nav-bg');
-        } else {
-            $('.navigation').removeClass('nav-bg');
-        }
-    });
+    
 
     // Background-images
     $('[data-background]').each(function () {
@@ -149,6 +142,8 @@
 })(jQuery);
 
 
+// Tipeo tipografia Hero
+
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -206,6 +201,7 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
+
 $( () => {
     
     //Click Logo To Scroll To Top
@@ -224,3 +220,42 @@ $( () => {
     });
     
 });
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}

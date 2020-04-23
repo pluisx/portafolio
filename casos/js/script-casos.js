fbc9d07f6b0f0d3010502a7ea9b9f514a5fe6623
pluisx <pluisx@gmail.com>
2020-04-23 12:02:26 -0300

    elimine imagenes sin uso y act de caso seekr

--- casos/js/script-casos.js
+++ casos/js/script-casos.js
@@ -1,3 +1,15 @@
+$( () => {
+    
+    //Smooth Scrolling Using Navigation Menu
+    $('a[href*="#"]').on('click', function(e){
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        },500);
+        e.preventDefault();
+    });
+    
+});
+
 
   // Esconder el menu en scrolldown
   var didScroll;

4545e9987919e034ad7afcf8814f2097a11fe8ca
pluisx <pluisx@gmail.com>
2020-04-24 01:19:26 -0300

    nuevas secciones en el caso seekr

--- casos/js/script-casos.js
+++ casos/js/script-casos.js
@@ -61,6 +61,50 @@ $( () => {
       type: 'doughnut',
       data: {
           datasets: [{
+              data: [52, 42, 6,],
+              backgroundColor: [
+                  '#4e70f8',
+                  '#5FA8F2',
+                  '#70CDF7',
+              ],
+              borderColor: [
+                '#fff',
+                '#fff',
+                '#fff',
+              ],
+              hoverBackgroundColor: [
+                'rgba(67, 98, 222, 100)',
+              ],
+              hoverBorderColor: [
+                '#fff',
+                '#fff',
+                '#fff',
+              ],
+          }]
+      },
+  
+      options: {
+        circumference: Math.PI,
+        rotation: -Math.PI,
+        tooltips: {
+          enabled: false,
+        },
+      },
+     
+  });
+  
+  
+  // Grafico der
+  var ctx = document.getElementById('myChart2').getContext('2d');
+  
+  var randomScalingFactor = function() {
+              return Math.round(Math.random() * 100);
+          };
+  
+  var myDoughnutChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+          datasets: [{
               data: [12, 6, 5, 3, 2, 2],
               backgroundColor: [
                   '#4e70f8',
@@ -102,31 +146,3 @@ $( () => {
      
   });
   
-  
-  // Grafico der
-  var ctx = document.getElementById('myChart2').getContext('2d');
-  var myDoughnutChart = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-          labels: ['Alto', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  '#4e70f8',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                '#fff'
-              ],
-              hoverBackgroundColor: [
-                'rgba(67, 98, 222, 100)',
-              ],
-          }]
-      },
-     
-  });

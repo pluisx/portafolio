5c33f168b33321a606df37cbc35026026fb12cfd
Pluisx <Pluisx@MacBook-Pro-de-Pluisx.local>
2020-05-11 12:11:47 -0400

    modificaciones contact form

--- contactform.php
+++ contactform.php
@@ -1,14 +1,19 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+    if(isset($_POST['submit'])){
+        $name=$_POST['name'];
+        $mailFrom=$_POST['email'];
+        $message=$_POST['message'];
 
-    $mailTo = "hola@pluisx.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "Haz recibido un correo de ".$name.".\n\n".$massage;
+        $mailTo="hola@pluisx.com";
+        $subject='Formulario de Pluisx.com'
+        $headers="From: ".$mailFrom;
+        $message="Haz recibido un correo de ".$name.".\n\n".$massage;
 
-    mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend");
-}
+        if(mail($mailTo, $subject, $message, $headers)){
+            echo "<h1>Mensaje enviado correctamente! Gracias"." ".$name.", te contactaremos lo antes posible.</h1>";
+        }
+        else{
+            echo "Algo salió mal!";
+        }
+    }

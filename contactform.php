c0c9d9e94af6549dd9c39476bb0dd2284e3795e3
Pluisx <Pluisx@MacBook-Pro-de-Pluisx.local>
2020-05-11 11:27:57 -0400

    new contact form

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "hola@pluisx.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Haz recibido un correo de ".$name.".\n\n".$massage;
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.php?mailsend")
+}

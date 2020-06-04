d5f7311cc330519402bd6db682d3d27b8916f6ad
Pedro Requena Perdomo <pluisx@gmail.com>
2020-06-04 11:00:55 -0400

    icons hero

--- form-portfolio.php
+++ form-portfolio.php
@@ -2,7 +2,7 @@
     $toEmail = "pluisx@gmail.com";
     $mailHeaders = "Mensaje de " . $_POST["userName"] . " ". $_POST["userEmail"] ."\r\n" . " ". $_POST["content"];
     if(mail($toEmail, $_POST["content"], $mailHeaders)) {
-        print "<p class='success'>Mensaje enviado correctamente. <i class='ti-check-box'></i></p>";
+        print "<p class='success text-center'>Mensaje enviado correctamente <i class='ti-check-box'></i></p>";
     } else {
         print "<p class='Error'>Hubo un problema al enviar tu mensaje.</p>";
     }
